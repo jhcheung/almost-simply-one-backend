98623756692ebe117831acf52b2ed1f3373a6d1a
Jimmy Cheung <jimmy.h.cheung@gmail.com>
2020-01-09 18:49:41 -0500

    setup first models and gems

---
+++ db/migrate/20200109234502_create_user_games.rb
@@ -0,0 +1,11 @@
+class CreateUserGames < ActiveRecord::Migration[6.0]
+  def change
+    create_table :user_games do |t|
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :game, null: false, foreign_key: true
+      t.integer :points
+
+      t.timestamps
+    end
+  end
+end
